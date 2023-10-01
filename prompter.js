dfb42d19fb4e5c9789b8221f29152c918942ac93
Tanish32 <110172756+Tanish32@users.noreply.github.com>
2023-10-01 17:46:57 +0530

    Update prompter.js

--- prompter.js
+++ prompter.js
@@ -1,4 +1,6 @@
-const Users = require('./registration.js')
+
+const Registration = require('./registration.js')
+const Users  = Registration.Users
 const { Blockchain, Transaction} = require("./blockchain");
 var bc = new Blockchain();
 const prompt = require('prompt-sync')();
@@ -6,28 +8,27 @@ const EC=require('elliptic').ec;
 const ec=new EC('secp256k1');
 const util = require('util');
 
-// console.log(util.inspect(Users,{depth:null}))
 
-/* ****error
-function toFromVerification(fromKey, toKey) { 
-    if (Users.Clients.some(pubKey => fromKey === pubKey)) return false;
-    
-    if (Users.Manufacturers.some(pubKey => fromKey === pubKey)) {
-        return Users.Distributors.some(pubKey1 => toKey === pubKey1);
+function toFromVerification(fromKey, toKey){
+    // Clients can't sell products
+    if(Users.Clients.some(client => client.key === fromKey)) return false;
+
+    // Manufacturer can only sell to a distributor
+    if(Users.Manufacturers.some(manufacturer => manufacturer.key === fromKey)){
+        return Users.Distributors.some(distributor => distributor.key === toKey);
     }
-    
-    if (Users.Distributors.some(pubKey => fromKey === pubKey)) {
-        return Users.Clients.some(pubKey1 => toKey === pubKey1);
+
+    // Distributor can only sell to a client
+    if(Users.Distributors.some(distributor => distributor.key === fromKey)){
+        return Users.Clients.some(client => client.key === toKey);
     }
+
     return false;
 }
 
-
-
 function isAddDistributor(fromPublicKey) {
-    return Users.Distributors.some(dist => fromPublicKey === dist);
+    return Users.Distributors.some(distributor => distributor.key === fromPublicKey) || false;
 }
-*/
 
 //    console.log(('Click 0: Add transactions\nClick 1: See transactions added\nClick 2: Start mining block\nClick 3: confirm delivery\nClick 4: print blockchain\nClick 5: distributor initiates delivery\nClick 6: QR code status\nClick 7: distributor confirms dispatch\nClick 8: issue with delivery (only if you have not received the product)\nClick 9: Exit\n'))
 
@@ -41,23 +42,25 @@ while(1){
 
             //rn we are taking public key and private key so that toFromVerification works we can make it better by just taking name and priate key 
             fromPublicKey= prompt('From user public key: ')
-            toKey = prompt('To user public key: ')
+            toPublicKey = prompt('To user public key: ')
             productID = prompt('ProductID: ')
             //verify to and from constraints
 
 
 
-            // if(!toFromVerification(fromPublicKey,toKey))  throw new Error("M->D->C")
+            if(!toFromVerification(fromPublicKey,toPublicKey))  throw new Error("M->D->C")
             fromPrivateKey = prompt('From user private key: ')
             amount = prompt('Enter amount: ')
 
-            // //to confirm if distributer has product
-            // if(!isAddDistributor(fromPublicKey) || !bc.checkFromAdressHoldsProduct(productID,fromPublicKey)){
-            //     console.log("Distrubuter doesnot have the product!")
-            //     break;
-            // }
+            //to confirm if distributer has product
+            if(isAddDistributor(fromPublicKey) ){
+                if(!bc.checkFromAdressHoldsProduct(productID,fromPublicKey)){
+                    console.log("Distrubuter doesnot have the product!")
+                    break;
+                }
+            }
 
-            tx1 = new Transaction(fromPublicKey,toKey,amount,productID)
+            tx1 = new Transaction(fromPublicKey,toPublicKey,amount,productID)
             const fromKey=ec.keyFromPrivate(fromPrivateKey);
             tx1.signTransaction(fromKey)
             bc.addTransaction(tx1)
